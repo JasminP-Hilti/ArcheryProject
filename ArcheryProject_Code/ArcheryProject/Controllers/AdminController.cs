e82e4298e8b02c9ddf39347925be550d6009e725
Badhofer Lisa <Lisa.Badhofer@BSLINZ2-IT.LOCAL>
2023-12-18 09:18:41 +0100

    body and borderradius

---
+++ ArcheryProject_Code/ArcheryProject/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using ArcheryProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ArcheryProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Play()
+        {
+            return View();
+        }
+
+        public IActionResult Stats()
+        {
+            return View();
+        }
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ArcheryProject_Code/ArcheryProject/wwwroot/js/login.js
@@ -0,0 +1,36 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("loginButton").addEventListener("click", switchForm);
+    document.getElementById("registerButton").addEventListener("click", switchForm);
+});
+
+
+
+function switchForm(event) {
+    const clickedButton = event.target;
+    console.log(clickedButton.id)
+    if (clickedButton.classList.contains("btn-inactive") == true) {
+        let activeButton = clickedButton.id === 'loginButton' ? document.getElementById("registerButton") : document.getElementById("loginButton");
+
+        activeButton.classList.remove("btn-primary");
+        activeButton.classList.add("btn-inactive");
+
+        clickedButton.classList.remove("btn-inactive");
+        clickedButton.classList.add("btn-primary");
+
+        const showFormId = clickedButton.id === 'loginButton' ? 'loginForm' : 'registerForm';
+        const hideFormId = clickedButton.id === 'loginButton' ? 'registerForm' : 'loginForm';
+        showForm(showFormId, hideFormId);
+    }
+}
+function showForm(showId, hideId) {
+    console.log("show " + showId + "hide " + hideId);
+    document.getElementById(showId).style.display = 'block';
+    document.getElementById(hideId).style.display = 'none';
+}
+
