6553f30c40b3596601e07db3791bf1d94de846b4
Lisa Badhofer <xlisax94@gmail.com>
2023-12-20 15:25:23 +0100

    upadate

---
+++ ArcheryProject_Code/ArcheryProject/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using ArcheryProject.Models;
+using Microsoft.AspNetCore.Mvc;
+//Home
+//Login
+
+namespace ArcheryProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult LoginOrRegister()
+        {
+            return View(new LoginRegisterModel());
+        }
+    }
+}
