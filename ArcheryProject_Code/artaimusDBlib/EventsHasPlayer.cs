5e5d48e88a899a990024a99c84941ea5a004281f
Freddy <fredi.f5000@gmail.com>
2023-12-21 15:00:28 +0100

    Added artaimusDBlib

---
+++ ArcheryProject_Code/artaimusDBlib/EventsHasPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArcheryProject;
+
+public partial class EventsHasPlayer
+{
+    public int EventsId { get; set; }
+
+    public int PlayersId { get; set; }
+
+    public int PointsTotal { get; set; }
+
+    public virtual Event Events { get; set; } = null!;
+
+    public virtual Player Players { get; set; } = null!;
+}
