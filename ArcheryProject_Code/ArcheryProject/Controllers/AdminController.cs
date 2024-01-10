ca2cf861fb55bca008a0de7c6ff96a6120acc8ab
Caroline Bär <caroline.baer@students.bs-linz2.ac.at>
2024-01-10 15:10:16 +0100

    modified Statistic Site

--- ArcheryProject_Code/ArcheryProject/Controllers/AdminController.cs
+++ ArcheryProject_Code/ArcheryProject/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using ArcheryProject.Models;
 using artaimusDBlib;
 using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.Esf;
 using System.Diagnostics;
 
 namespace ArcheryProject.Controllers
@@ -30,13 +31,74 @@ namespace ArcheryProject.Controllers
 
         public IActionResult Stats()
         {
-            return View();
+
+            List<StatisticModel> tmpModels = new List<StatisticModel>();
+
+            var playersList = dbCtx.Players.ToList();
+            var parcourList = dbCtx.Parcours.ToList();
+            var eventList= dbCtx.Events.ToList();
+
+            foreach (var eventPlayer in dbCtx.EventsHasPlayers)
+            {
+                var player = playersList.FirstOrDefault(p => p.Id == eventPlayer.PlayersId /*&& p.Id ==2*/);
+                var eventInfo = eventList.FirstOrDefault(e => e.Id == eventPlayer.EventsId);
+
+
+
+                if (player != null && eventInfo != null)
+                {
+                    var parcour = parcourList.FirstOrDefault(p => p.Id == eventInfo.ParcoursId);
+
+                    if(parcour != null)
+                    { 
+                    tmpModels.Add(new StatisticModel
+                    {
+                        FirstName = player.FirstName,
+                        LastName = player.LastName,
+                        Nickname = player.Nickname,
+                        PlayersId = eventPlayer.PlayersId,
+                        EventsId = eventPlayer.EventsId,
+                        PointsTotal = eventPlayer.PointsTotal,
+                        ParcoursName = parcour.Name,
+                        ParcoursId = parcour.Id
+                                             
+                    });
+                    }
+                }
+            }
+
+            return View(tmpModels);
+
+            
+
+            //List<EventHasPlayerModel> tmpModels = new List<EventHasPlayerModel>();
+
+            //foreach (var tmpPoints in dbCtx.EventsHasPlayers)
+            //{
+            //    foreach (var player in dbCtx.Players)
+            //    {
+
+            //    }
+
+            //    tmpModels.Add(new EventHasPlayerModel
+            //    {
+            //        PlayersId = tmpPoints.PlayersId,
+            //        EventsId = tmpPoints.EventsId,
+            //        PointsTotal = tmpPoints.PointsTotal
+
+            //    });
+
+            //}
+
+            //return View(tmpModels);
+
+
         }
         public IActionResult Admin()
         {
             List<ParcourModel> tmpMOdels = new List<ParcourModel>();
 
-            foreach(var tmpPar in dbCtx.Parcours)
+            foreach (var tmpPar in dbCtx.Parcours)
             {
                 tmpMOdels.Add(new ParcourModel
                 {
---
+++ ArcheryProject_Code/ArcheryProject/Models/StatisticModel.cs
@@ -0,0 +1,21 @@
+﻿namespace ArcheryProject.Models
+{
+    public class StatisticModel
+    {
+        public int EventsId { get; set; }
+        public int PlayersId { get; set; }
+        public int PointsTotal { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+        public string? Nickname { get; set; }
+        public int ParcoursId { get; set; }
+
+        public string? ParcoursName { get; set; }
+
+
+    }
+
+  
+}
