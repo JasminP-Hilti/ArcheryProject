3dc681b56323cb55c9ca6b83cf42ed8b7151e082
Lisa Badhofer <xlisax94@gmail.com>
2024-01-17 10:14:51 +0100

    added playercontroller & fixed relinks with playerdata

---
+++ ArcheryProject_Code/ArcheryProject/Controllers/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using ArcheryProject.Models;
+using artaimusDBlib;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArcheryProject.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly ILogger<PlayerController> _logger;
+
+        private ArtaimusContext dbCtx;
+        public List<PlayerModel> tmpPlayers = new List<PlayerModel>();
+
+        public PlayerController(ILogger<PlayerController> logger, ArtaimusContext dbCtx)
+        {
+            _logger = logger;
+
+            this.dbCtx = dbCtx;
+        }
+
+        public IActionResult Index(PlayerModel player)
+        {
+            return View(player);
+        }
+
+        public IActionResult Play(PlayerModel player)
+        {
+            //Get logged in Player from DB
+            return View(player);
+        }
+        public IActionResult Stats(PlayerModel player)
+        {
+            return View(player);
+        }
+    }
+}
